1444721c1a2ec48e993a3e8feb2821e861cdb030
Carl Halen <carl.halen@elev.ga.ntig.se>
2023-10-06 11:03:00 +0200

    ini

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Namespace;
+Console.WriteLine("Welcome to the Tamagotchi");
+
+Tamagotchi caly = new Tamagotchi();
+
+Console.WriteLine ("Choose a new name for your Tamagotchi");
+
+caly.name = Console.Readline();
+
+Console.WriteLine($"{caly.name} is a good name!");
+
---
+++ ConsoleApp1/Tamagotchi.cs
@@ -0,0 +1,16 @@
+public class Tamagotchi
+{
+
+    private int hunger;
+    private int boredom;
+    private List<string> words = new List<string>() {"Welcome"}; 
+    private bool isAlive;
+    private Random generator; 
+    public string name; 
+
+    public Tomagotchi()
+    {
+    isAlive = true;
+    }
+    
+}
